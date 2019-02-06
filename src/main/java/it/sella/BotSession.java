f7c60e7f937ec7d8fc5ee51c9c8ead03302822d1
udayanid <udhayan.addon@gmail.com>
2019-02-07 01:34:42 +0530

    Added HttpSession Object for IM

---
+++ src/main/java/it/sella/BotSession.java
@@ -0,0 +1,49 @@
+package it.sella;
+
+public class BotSession {
+
+	private String fbSenderId;
+	private String fbReceipientId;
+	private String imChatId;
+	private String cokkieInfo;
+
+	public String getCokkieInfo() {
+		return cokkieInfo;
+	}
+
+	public void setCokkieInfo(String cokkieInfo) {
+		this.cokkieInfo = cokkieInfo;
+	}
+
+	public String getFbSenderId() {
+		return fbSenderId;
+	}
+
+	public void setFbSenderId(String fbSenderId) {
+		this.fbSenderId = fbSenderId;
+	}
+
+	public String getFbReceipientId() {
+		return fbReceipientId;
+	}
+
+	public void setFbReceipientId(String fbReceipientId) {
+		this.fbReceipientId = fbReceipientId;
+	}
+
+	public String getImChatId() {
+		return imChatId;
+	}
+
+	public void setImChatId(String imChatId) {
+		this.imChatId = imChatId;
+	}
+
+	@Override
+	public String toString() {
+		return "BotSession [fbSenderId=" + fbSenderId + ", fbReceipientId=" + fbReceipientId + ", imChatId=" + imChatId
+				+ ", cokkieInfo=" + cokkieInfo + "]";
+	}
+
+	
+}
