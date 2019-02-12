3516bf62275a307a86a84b3bef683aa834868127
udayanid <udhayan.addon@gmail.com>
2019-02-12 09:44:52 +0530

    expiration of cookie checking

--- src/main/java/it/sella/controller/SellaFbController.java
+++ src/main/java/it/sella/controller/SellaFbController.java
@@ -99,8 +99,15 @@ public class SellaFbController {
 				senderActionAcknowledge = sendMessage(getSenderActionResonse("typing_on", senderId));
 				logger.info("<<<<<<<<<<<<<senderActionAcknowledge::::{}>>>>>>>>>>>>>>", senderActionAcknowledge);
 				logger.info("<<<<<<<<<<<<<ChatId As of Now::::{}>>>>>>>>>>>>>>", botSession.getImChatId());
+				try {
+					getPollResponse(senderId, botSession.getImChatId(),botSession.getCokkieInfo().concat("hello"),1);
+
+				}catch(Exception e) {
+					logger.info("this is the error we got::{}",e.getMessage(),e);
+				}
 				ChatResponse chatResponse = sendImMessage(botSession.getImChatId(), textMessage, botSession.getCokkieInfo()).getBody();
 				logger.info("<<<<<<<<<ChatResponse:::{}",chatResponse);
+				
 				if(chatResponse.getStatus().equals("EXCEPTION")) {
 					final String chatId = getChatId(botSession.getCokkieInfo());
 	            	botSession.setImChatId(chatId);
